f051e2379857b9f4881225d345f1d22d41642dd3
Andrew DuBrock <adubrock@comcast.net>
2018-03-20 00:46:23 -0700

    another tweak

--- public/push-worker.js
+++ public/push-worker.js
@@ -1,10 +1,11 @@
+var userData = require('../user_data.js');
 // 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1521521391.3
 importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
-  defaultIcon: 'https://avatars3.githubusercontent.com/u/4516009?s\u003D400\u0026v\u003D4',
+  defaultIcon: userData.defaultIcon,
   defaultTitle: 'Excellent Notification',
   defaultActionURL: 'https://www.google.com',
-  appKey: 'rkOT1QWDSDKLjUV3qytVfQ',
-  token: 'MTpya09UMVFXRFNES0xqVVYzcXl0VmZROkw1TFR3b2RrVnh6bHNaZDVjaWdUaVdMOWY2WklqSWM5aHpSd19xNDhBM1U',
-  vapidPublicKey: 'BEczXifCdkloJcUn71OFhAc6_CW2zueGGaj_eWKaA4WRi7Z3pm9eMM8RwKwzOVO_tEKahmVLM_U6DQgGN1uJAYE='
+  appKey: userData.appKey,
+  token: userData.token,
+  vapidPublicKey: userData.vapidPublicKey
 })
