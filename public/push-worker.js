61d89ad85c8b0455a90d93b61bcb7438fb000985
Andrew DuBrock <adubrock@comcast.net>
2018-03-19 22:22:41 -0700

    added push worker and reg button

---
+++ public/push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1521521391.3
+importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://avatars3.githubusercontent.com/u/4516009?s\u003D400\u0026v\u003D4',
+  defaultTitle: 'Excellent Notification',
+  defaultActionURL: 'https://www.google.com',
+  appKey: 'rkOT1QWDSDKLjUV3qytVfQ',
+  token: 'MTpya09UMVFXRFNES0xqVVYzcXl0VmZROkw1TFR3b2RrVnh6bHNaZDVjaWdUaVdMOWY2WklqSWM5aHpSd19xNDhBM1U',
+  vapidPublicKey: 'BEczXifCdkloJcUn71OFhAc6_CW2zueGGaj_eWKaA4WRi7Z3pm9eMM8RwKwzOVO_tEKahmVLM_U6DQgGN1uJAYE='
+})
